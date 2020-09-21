918e4a569a04e4f5e5916e7f012499ba11bda8cf
Quan Nguyen <quannguyen234vn@gmail.com>
2020-09-21 10:51:01 +0700

    Updates

---
+++ precache-manifest.04ae84c55d6ef63c1898ed93dfad5f80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75fde5a382e1a660dc5fa517678e06b",
+    "url": "/MonstersCollection/index.html"
+  },
+  {
+    "revision": "c30525b03ec245d3d549",
+    "url": "/MonstersCollection/static/css/main.41167764.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/MonstersCollection/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersCollection/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c30525b03ec245d3d549",
+    "url": "/MonstersCollection/static/js/main.75d5e800.chunk.js"
+  },
+  {
+    "revision": "cc4dd0e188cbc153fa08",
+    "url": "/MonstersCollection/static/js/runtime-main.7d7bd9ce.js"
+  }
+]);
